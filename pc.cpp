1b0427c26c645143cc4ddcdd8cdff35a42e5fc8a
nickwid <nicholasrwidmer@icloud.com>
2023-11-07 18:04:15 -0500

    updated code

--- pc.cpp
+++ pc.cpp
@@ -13,21 +13,21 @@ sem_t semFull;
 
 pthread_mutex_t mutexBuffer;
 
-int buffer[10];
-int count = 0;
+int buffer[10]; //creating buffer size
+int count = 0; //start count from 0
 
 void* producer(void* args){
     while(1){
     //produce
-    int x = rand() % 100;
+    int x = rand() % 100; //gen rand num to be put in buffer
     //sleep(1);
 
     //add to buffer 
     sem_wait(&semEmpty); //see if buffer has room to produce
-    pthread_mutex_lock(&mutexBuffer); 
+    pthread_mutex_lock(&mutexBuffer); //lock while modifying
     buffer[count] = x;
     count++; 
-    pthread_mutex_unlock(&mutexBuffer);
+    pthread_mutex_unlock(&mutexBuffer); //unlock when done modifying the buffer
     sem_post(&semFull);
     }
 }
@@ -55,7 +55,7 @@ int main(int argc, char* argv[]){
     srand(time(NULL));
     pthread_t th[THREAD_NUM];
     pthread_mutex_init(&mutexBuffer, NULL);
-    sem_init(&semEmpty, 0, 10);
+    sem_init(&semEmpty, 0, 10); //initializing the num of slots available at the beggining
     sem_init(&semFull, 0, 0);
     for (int i = 0; i < THREAD_NUM; i++){
         if(i > 0){
@@ -73,7 +73,7 @@ int main(int argc, char* argv[]){
             perror("failed to create thread");
         }
     }
-    sem_destroy(&semEmpty);
+    sem_destroy(&semEmpty); //destroy the sems once done
     sem_destroy(&semFull);
     pthread_mutex_destroy(&mutexBuffer);
     return 0;
